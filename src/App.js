955d10c40a8d3eccec874ec85705518e48bf47c6
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-07-22 20:22:14 +0000

    Input access and refresh tokens with axios

--- src/App.js
+++ src/App.js
@@ -5,45 +5,22 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
-import { createContext, useEffect, useState } from "react";
-import axios from "axios";
 
-export const CurrentUserContext = createContext()
-export const setCurrentUserContext = createContext()
 
 function App() {
 
-	const [currentUser, setCurrentUser] = useState(null);
-
-	const handleMount = async () => {
-		try {
-			const {data} = await axios.get('dj-rest-auth/user/');
-		} catch (err) {
-			console.log(err)
-		}
-	}
-
-	useEffect(() => {
-		handleMount()
-	}, [])
-
 	return (
-		<CurrentUserContext.Provider value={currentUser}>
-			<setCurrentUserContext.Provider value={setCurrentUser}>
-				<div className={styles.App}>
-					<NavBar />
-						<Container className={styles.Main}>
-						<Switch>
-							<Route exact path="/" render={() => <h1>Home page</h1>} />
-							<Route exact path="/signin" render={() => <SignInForm />} />
-							<Route exact path="/signup" render={() => <SignUpForm />} />
-							<Route render={() => <h1>Page not found!</h1>} />
-						</Switch>
-					</Container>
-				</div>
-			</setCurrentUserContext.Provider>
-		</CurrentUserContext.Provider>
-
+		<div className={styles.App}>
+			<NavBar />
+			<Container className={styles.Main}>
+				<Switch>
+					<Route exact path="/" render={() => <h1>Home page</h1>} />
+					<Route exact path="/signin" render={() => <SignInForm />} />
+					<Route exact path="/signup" render={() => <SignUpForm />} />
+					<Route render={() => <h1>Page not found!</h1>} />
+				</Switch>
+			</Container>
+		</div>
 	);
 }
 
