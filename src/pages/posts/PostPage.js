41bfce197775bfce1ef8231ed15024ab48bda2bc
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-07-24 21:54:57 +0000

    Added edit and infinite scroll to Comment component

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
 
-import CommentCreateForm from "../comments/CommentCreateForm";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
-
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import Comment from "../comments/Comment";
 
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostPage() {
     const { id } = useParams();
@@ -27,7 +30,7 @@ function PostPage() {
             try {
                 const [{ data: post }, { data: comments }] = await Promise.all([
                     axiosReq.get(`/posts/${id}`),
-                    axiosReq.get(`/comments/?post=${id}`)
+                    axiosReq.get(`/comments/?post=${id}`),
                 ]);
                 setPost({ results: [post] });
                 setComments(comments);
@@ -57,14 +60,20 @@ function PostPage() {
                         "Comments"
                     ) : null}
                     {comments.results.length ? (
-                        comments.results.map((comment) => (
-                            <Comment
-                                key={comment.id}
-                                {...comment}
-                                setPost={setPost}
-                                setComments={setComments}
-                            />
-                        ))
+                        <InfiniteScroll
+                            children={comments.results.map((comment) => (
+                                <Comment
+                                    key={comment.id}
+                                    {...comment}
+                                    setPost={setPost}
+                                    setComments={setComments}
+                                />
+                            ))}
+                            dataLength={comments.results.length}
+                            loader={<Asset spinner />}
+                            hasMore={!!comments.next}
+                            next={() => fetchMoreData(comments, setComments)}
+                        />
                     ) : currentUser ? (
                         <span>No comments yet, be the first to comment!</span>
                     ) : (
@@ -75,7 +84,7 @@ function PostPage() {
             <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
                 Popular profiles for desktop
             </Col>
-        </Row >
+        </Row>
     );
 }
 
