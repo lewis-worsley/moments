e178ab34556850439368e241a390d7066af139f5
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-07-25 11:05:38 +0000

    Added Profile component to display profile avatars, links, follow etc

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -4,6 +4,7 @@ import appStyles from '../../App.module.css';
 import { axiosReq } from '../../api/axiosDefaults';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import Asset from '../../components/Asset';
+import Profile from './Profile';
 
 
 const PopularProfiles = ({ mobile }) => {
@@ -42,13 +43,13 @@ const PopularProfiles = ({ mobile }) => {
                     {mobile ? (
                         <div className='d-flex justify-content-around'>
                             {popularProfiles.results.slice(0, 4).map((profile) => (
-                                <p key={profile.id}>{profile.owner}</p>
+                                <Profile key={profile.id} profile={profile} mobile />
                             ))}
                         </div>
                     ) : (
                         popularProfiles.results.map((profile) => (
-                            <p key={profile.id}>{profile.owner}</p>
-                        ))
+                            <Profile key={profile.id} profile={profile} />
+                            ))
                     )}
                 </>
             ) : (
