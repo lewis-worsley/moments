c39f5eba2c2a2f84fc09a433d03608bf8973f0ad
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-07-27 20:21:26 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
-import { Route } from "react-router-dom";
-import { Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
@@ -19,39 +18,39 @@ import NotFound from "./components/NotFound";
 
 function App() {
 	const currentUser = useCurrentUser();
-	const profile_id = currentUser?.profile_id || ""
+	const profile_id = currentUser?.profile_id || "";
 
 	return (
 		<div className={styles.App}>
 			<NavBar />
 			<Container className={styles.Main}>
 				<Switch>
-					<Route 
-						exact 
-						path="/" 
+					<Route
+						exact
+						path="/"
 						render={() => (
 							<PostsPage message="No results found. Adjust the search keyword." />
-						)} 
+						)}
 					/>
-					<Route 
-						exact 
-						path="/feed" 
+					<Route
+						exact
+						path="/feed"
 						render={() => (
-							<PostsPage 
-								message="No results found. Adjust the search keyword or follow a user." 
+							<PostsPage
+								message="No results found. Adjust the search keyword or follow a user."
 								filter={`owner__followed__owner__profile=${profile_id}&`}
 							/>
-						)} 
+						)}
 					/>
-					<Route 
-						exact 
-						path="/liked" 
+					<Route
+						exact
+						path="/liked"
 						render={() => (
-							<PostsPage 
-								message="No results found. Adjust the search keyword or like a post." 
+							<PostsPage
+								message="No results found. Adjust the search keyword or like a post."
 								filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
-								/>
-						)} 
+							/>
+						)}
 					/>
 					<Route exact path="/signin" render={() => <SignInForm />} />
 					<Route exact path="/signup" render={() => <SignUpForm />} />
@@ -59,9 +58,22 @@ function App() {
 					<Route exact path="/posts/:id" render={() => <PostPage />} />
 					<Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
 					<Route exact path="/profiles/:id" render={() => <ProfilePage />} />
-					<Route exact path="/profiles/:id/edit/username" render={() => <UsernameForm />} />
-					<Route exact path="/profiles/:id/edit/password" render={() => <UserPasswordForm />} />
-					<Route exact path="/profiles/:id/edit" render={() => <ProfileEditForm />} />
+					<Route
+						exact
+						path="/profiles/:id/edit/username"
+						render={() => <UsernameForm />}
+					/>
+					<Route
+						exact
+						path="/profiles/:id/edit/password"
+						render={() => <UserPasswordForm />}
+					/>
+					<Route
+						exact
+						path="/profiles/:id/edit"
+						render={() => <ProfileEditForm />}
+					/>
+
 					<Route render={() => <NotFound />} />
 				</Switch>
 			</Container>
